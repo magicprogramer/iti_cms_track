217146dad2a7b0e8f54fc4330a900b1d714debc4
ahmed ezzat <ahmedalahly800@gmail.com>
2024-06-02 22:38:41 +0300

    updated task 4

--- js/day2/task4.js
+++ js/day2/task4.js
@@ -1,5 +1,7 @@
 let r = parseFloat(prompt("enter the radius value"));
 alert("Total area of the circle is " + (r * Math.PI * r));
+let numb = parseInt(prompt("enter a number "));
+alert("sqrt of " + numb + " = " + Math.round(Math.sqrt(numb)));
 let angle = parseFloat(prompt("enter angle"));
 let red_angle = angle / 180 * Math.PI;
 console.log(red_angle);
