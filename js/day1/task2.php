8f17b3fb757a86b4ead2cc3becd27836e3ef8b46
ahmed ezzat <ahmedalahly800@gmail.com>
2024-06-23 12:48:32 +0300

    php tasks day 1

---
+++ js/day1/task2.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+</head>
+<body>
+    <?php
+    define("WEBSITE_NAME", "thewebsite");
+    echo WEBSITE_NAME;
+    ?>
+</body>
+    </html>
---
+++ js/day1/task3.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+</head>
+<body>
+    <?php
+    $age = 10;
+    switch($age)
+    {
+        case $age < 5:
+            echo "Stay at Home";
+            break;
+        case $age == 5:
+            echo "Go to Kindergarden,";
+            break;
+        case $age > 5 && $age <= 12 :
+            echo "Go to grade ".( $age - 5);
+            break;
+    }
+    ?>
+</body>
+    </html>
